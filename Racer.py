35d0cc28e328accd3ddbb6242c68577b976c43d0
VŨ NGUYỄN LONG <20120405@student.hcmus.edu.vn>
2020-12-08 00:02:05 +0700

    Initlize source

---
+++ Racer.py
@@ -0,0 +1,35 @@
+# import pygame
+
+# display_width = 1080
+# display_height = 720
+
+# FPS = 144
+# fpsClock = pygame.time.Clock()
+
+# gameDisplay = pygame.display.set_mode((display_width, display_height))
+# pygame.display.set_caption('Racer')
+
+# class car1():
+
+
+# def game():
+#     while True:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 quit()
+
+#         gameDisplay.fill((0,0,0))
+
+#         pygame.display.update()
+#         fpsClock.tick(FPS)
+
+# #Main
+# game()  
+
+k = 1
+
+class Test:
+    k = 2
+
+hello = Test
+print(k)
